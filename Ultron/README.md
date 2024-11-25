8182f6f6b7de3a4a95784f39971cde30f0bbe945
parasomni <parasomni@proton.me>
2024-11-25 11:33:14 +0000

    add Ultron multi tool

---
+++ Ultron/README.md
@@ -0,0 +1,10 @@
+## About Ultron
+Ultron is a multi tool including the following features:
+- integrity script
+- password generator (deprecated)
+- denial of service script
+- backup tool
+- hash script
+- email spam / automated email script
+- ARP spoofer
+- keylogger
---
+++ Ultron/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import os
+
+def check_dir(dirPath):
+    if os.path.exists(str(dirPath)):
+        print(f'Directory {dirPath} exists.')
+        pass
+    else:
+        print(f'Directory {dirPath} not exists --> creating...')
+        os.makedirs(dirPath)
+
+def make_dirs():
+    print("running prescan for directories...")
+    ultron_hashes = '/etc/ultron/ultron-hashes/'
+    ultron_logs = '/etc/ultron/ultron-logs'
+    ultron_dir_logs = '/etc/ultron//ultron-dir-logs'
+    ultron_main_logs = '/etc/ultron/main-logs'
+    check_dir(ultron_dir_logs)
+    check_dir(ultron_hashes)
+    check_dir(ultron_logs)
+    check_dir(ultron_main_logs)
+    print("setting configuration")
+    os.system("mv ultron.config /etc/ultron/ultron.config")
+    print("installing triggers")
+    os.system("mv ultron /etc/ultron/ultron")
+
+
+if __name__ == "__main__":
+    make_dirs()
